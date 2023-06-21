16050bb91b2cfb6ca18daba780d7d7932920cd37
masterpablo_repo <comtotal77@yahoo.com>
2023-06-20 23:19:11 -0400

    codigos_prueba_utiles

--- app.js
+++ app.js
@@ -28,17 +28,45 @@ app.get(':endpoint([\\/\\w\\.-]*)', async function (req, res) {
     // Remove any trailing slash from base url
     const endpoint = (process.env.API_BASE_URL).replace(/\/$/, "") + req.params.endpoint
 
-    
-    axios.get(endpoint, {
+    const regex2 = /^\/((?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?))\/(\w+)$/;
+    const regex = /^\/((?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?))\/(enable|disable)$/;
+    const matches = req.params.endpoint.match(regex);
+  
+    if (matches) {
+      const ip = matches[1];
+      const accion = matches[2];
+      const endpoint = (process.env.API_BASE_URL).replace(/\/$/, "") + "/ip/firewall/address-list"  
+      console.log("la ip es", ip);
+      console.log("la acción es", accion);
+      console.log("el endpoint es", endpoint);
+      const response =   await axios.get(endpoint, {
         httpsAgent: agent,
         headers: { 
             'Authorization': 'Basic dXNlcjE6MTQ3OTYzbGtqKio='
           }
     }).then(response => {
-        res.json(response.data)
+        //res.json(response.data)
+        const registros = response.data;
+        let output = "";
+        registros.forEach((registro, index) => {
+        const id = registro[".id"];
+        output += `Registro ${index + 1}: ID:${id}\n`;
+        });
+        res.send(output);
+        //res.send(response.data[".id"])
     }).catch(error => {
         res.json(error)
     })
+
+      //res.status(200).send("Operación exitosa");
+
+
+      // Resto del código de la API aquí
+    } else {
+      // Manejar la ruta de endpoint no válida
+      res.status(400).send("Ruta de endpoint no válida");
+    }
+
 })
 
 //Iniciando el servidor, escuchando...
